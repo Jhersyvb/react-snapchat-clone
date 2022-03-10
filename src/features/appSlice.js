aa0721931853bfa47d2bf0d91f2296beed68e478
Jhersy <jhersyvb@gmail.com>
2022-03-09 20:11:27 -0500

    Starting the build

---
+++ src/features/appSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+  status: 'idle',
+};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { incrementByAmount } = appSlice.actions;
+
+export const selectApp = (state) => state.app.value;
+
+export default appSlice.reducer;
