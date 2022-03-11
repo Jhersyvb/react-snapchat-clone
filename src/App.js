af7387dda55bbd2ff7342f6eb9147f177d6dd5f4
Jhersy <jhersyvb@gmail.com>
2022-03-10 20:38:36 -0500

    Setting up Firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import WebcamCapture from './components/WebcamCapture'
 
 function App() {
   return (
     <div className="app">
       <h1>LETS BUILD SNAPCHAT</h1>
+      <WebcamCapture />
     </div>
   )
 }
